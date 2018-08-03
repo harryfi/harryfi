0bb01f40b155a899805ccea036f5099e561cda3e
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2018-08-04 05:23:14 +0700

    Fix context MO

---
+++ MasterOnline/Migrations/201808032211271_IgnoreAttributeBlibli.cs
@@ -0,0 +1,16 @@
+namespace MasterOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IgnoreAttributeBlibli : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
