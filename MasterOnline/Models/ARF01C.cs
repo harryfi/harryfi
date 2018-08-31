d80f402c14e0476364ed1593558577bf0d13b792
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2018-08-31 08:21:16 +0700

    Fix 50 length email field

--- MasterOnline/Models/ARF01C.cs
+++ MasterOnline/Models/ARF01C.cs
@@ -170,7 +170,7 @@ namespace MasterOnline
         public string KODEPOS { get; set; }
 
         [Required]
-        [StringLength(30)]
+        [StringLength(50)]
         public string EMAIL { get; set; }
 
         [Required]
