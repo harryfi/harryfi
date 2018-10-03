1e95ff2c46a225a52710eb2a97466812aebaa589
calvin <cool.shinra123@gmail.com>
2018-10-03 10:27:34 +0700

    email registrasi

--- MasterOnline/Controllers/AccountController.cs
+++ MasterOnline/Controllers/AccountController.cs
@@ -426,15 +426,28 @@ namespace MasterOnline.Controllers
             message.Body = string.Format(body, account.Email, originPassword);
             message.IsBodyHtml = true;
 #if AWS
+            //using (var smtp = new SmtpClient())
+            //{
+            //    var credential = new NetworkCredential
+            //    {
+            //        UserName = "AKIAIXN2D33JPSDL7WEQ",
+            //        Password = "ApBddkFZF8hwJtbo+s4Oq31MqDtWOpzYKDhyVGSHGCEl"
+            //    };
+            //    smtp.Credentials = credential;
+            //    smtp.Host = "email-smtp.us-east-1.amazonaws.com";
+            //    smtp.Port = 587;
+            //    smtp.EnableSsl = true;
+            //    await smtp.SendMailAsync(message);
+            //}
             using (var smtp = new SmtpClient())
             {
                 var credential = new NetworkCredential
                 {
-                    UserName = "AKIAIXN2D33JPSDL7WEQ",
-                    Password = "ApBddkFZF8hwJtbo+s4Oq31MqDtWOpzYKDhyVGSHGCEl"
+                    UserName = "csmasteronline@gmail.com",
+                    Password = "erasoft123"
                 };
                 smtp.Credentials = credential;
-                smtp.Host = "email-smtp.us-east-1.amazonaws.com";
+                smtp.Host = "smtp.gmail.com";
                 smtp.Port = 587;
                 smtp.EnableSsl = true;
                 await smtp.SendMailAsync(message);
