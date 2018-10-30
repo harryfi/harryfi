45141b500683b9d40796f653ab64690d6dc83af6
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2018-10-30 14:48:25 +0700

    Fix bugs database menu in admin

--- MasterOnline/Controllers/AdminController.cs
+++ MasterOnline/Controllers/AdminController.cs
@@ -132,68 +132,70 @@ namespace MasterOnline.Controllers
 
         public ActionResult TambahHapusDatabaseAcc(int? accId)
         {
-            var accInDb = MoDbContext.Account.Single(a => a.AccountId == accId);
-            accInDb.Status = !accInDb.Status;
+            var accInDb = MoDbContext.Account.FirstOrDefault(a => a.AccountId == accId);
 
-            try
+            if (accInDb != null)
             {
-                //change by calvin 3 oktober 2018
-                //var userId = Convert.ToString(accInDb.UserId);
-                var userId = Convert.ToString(accInDb.AccountId);
-                //add by calvin 3 oktober 2018
-                accInDb.DatabasePathErasoft = "ERASOFT_" + userId;
-
-                string sql = "";
-
-                if (accInDb.Status)
+                try
                 {
-                    var path = Server.MapPath("~/Content/admin/");
-                    sql = $"RESTORE DATABASE {accInDb.DatabasePathErasoft} FROM DISK = '{path + "ERASOFT_backup_for_new_account.bak"}'" +
-                                 $" WITH MOVE 'erasoft' TO '{path}/{accInDb.DatabasePathErasoft}.mdf'," +
-                                 $" MOVE 'erasoft_log' TO '{path}/{accInDb.DatabasePathErasoft}.ldf';";
+                    //change by calvin 3 oktober 2018
+                    //var userId = Convert.ToString(accInDb.UserId);
+                    var userId = Convert.ToString(accInDb.AccountId);
+                    //add by calvin 3 oktober 2018
+                    accInDb.DatabasePathErasoft = "ERASOFT_" + userId;
+
+                    string sql = "";
+
+                    if (accInDb.Status)
+                    {
+                        var path = Server.MapPath("~/Content/admin/");
+                        sql = $"RESTORE DATABASE {accInDb.DatabasePathErasoft} FROM DISK = '{path + "ERASOFT_backup_for_new_account.bak"}'" +
+                                     $" WITH MOVE 'erasoft' TO '{path}/{accInDb.DatabasePathErasoft}.mdf'," +
+                                     $" MOVE 'erasoft_log' TO '{path}/{accInDb.DatabasePathErasoft}.ldf';";
 #if AWS
                     SqlConnection con = new SqlConnection("Server=localhost;Initial Catalog=master;persist security info=True;" +
                                                           "user id=masteronline;password=M@ster123;");
 #else
-                    SqlConnection con = new SqlConnection("Server=202.67.14.92\\SQLEXPRESS,1433;Initial Catalog=master;persist security info=True;" +
-                                                          "user id=masteronline;password=M@ster123;");
+                        SqlConnection con = new SqlConnection("Server=202.67.14.92\\SQLEXPRESS,1433;Initial Catalog=master;persist security info=True;" +
+                                                              "user id=masteronline;password=M@ster123;");
 #endif
-                    SqlCommand command = new SqlCommand(sql, con);
+                        SqlCommand command = new SqlCommand(sql, con);
 
-                    con.Open();
-                    command.ExecuteNonQuery();
-                    con.Close();
-                    con.Dispose();
+                        con.Open();
+                        command.ExecuteNonQuery();
+                        con.Close();
+                        con.Dispose();
 
 
-                    //add by Tri 20-09-2018, save nama toko ke SIFSYS
-                    //change by calvin 3 oktober 2018
-                    //ErasoftContext ErasoftDbContext = new ErasoftContext(userId);
-                    ErasoftContext ErasoftDbContext = new ErasoftContext(accInDb.DatabasePathErasoft);
-                    //end change by calvin 3 oktober 2018
-                    var dataPerusahaan = ErasoftDbContext.SIFSYS.FirstOrDefault();
-                    if (string.IsNullOrEmpty(dataPerusahaan.NAMA_PT))
-                    {
-                        dataPerusahaan.NAMA_PT = accInDb.NamaTokoOnline;
-                        ErasoftDbContext.SaveChanges();
+                        //add by Tri 20-09-2018, save nama toko ke SIFSYS
+                        //change by calvin 3 oktober 2018
+                        //ErasoftContext ErasoftDbContext = new ErasoftContext(userId);
+                        ErasoftContext ErasoftDbContext = new ErasoftContext(accInDb.DatabasePathErasoft);
+                        //end change by calvin 3 oktober 2018
+                        var dataPerusahaan = ErasoftDbContext.SIFSYS.FirstOrDefault();
+                        if (string.IsNullOrEmpty(dataPerusahaan.NAMA_PT))
+                        {
+                            dataPerusahaan.NAMA_PT = accInDb.NamaTokoOnline;
+                            ErasoftDbContext.SaveChanges();
+                        }
+                        //end add by Tri 20-09-2018, save nama toko ke SIFSYS
+
                     }
-                    //end add by Tri 20-09-2018, save nama toko ke SIFSYS
-
-                }
-                else
-                {
+                    else
+                    {
 #if AWS
                     System.Data.Entity.Database.Delete($"Server=localhost;Initial Catalog={accInDb.DatabasePathErasoft};persist security info=True;" +
                                                        "user id=masteronline;password=M@ster123;");
 #else
-                    System.Data.Entity.Database.Delete($"Server=202.67.14.92\\SQLEXPRESS,1433;Initial Catalog={accInDb.DatabasePathErasoft};persist security info=True;" +
-                                                       "user id=masteronline;password=M@ster123;");
+                        System.Data.Entity.Database.Delete($"Server=202.67.14.92\\SQLEXPRESS,1433;Initial Catalog={accInDb.DatabasePathErasoft};persist security info=True;" +
+                                                           "user id=masteronline;password=M@ster123;");
 #endif
+                    }
                 }
-            }
-            catch (Exception e)
-            {
-                return Content(e.Message);
+                catch (Exception e)
+                {
+                    return Content(e.Message);
+                }
             }
 
             var listAcc = MoDbContext.Account.ToList();
