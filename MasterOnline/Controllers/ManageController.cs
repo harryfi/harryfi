5ba7740c8c9fbadb66379cf2b01bbd842feeb1a2
suwantatri <fierywings5@gmail.com>
2018-12-13 14:05:03 +0700

    lanjutan tab barang paling laku

--- MasterOnline/Controllers/ManageController.cs
+++ MasterOnline/Controllers/ManageController.cs
@@ -1351,7 +1351,7 @@ namespace MasterOnline.Controllers
         {
             var listBarangLaku = new List<PenjualanBarang>();
             var qohqoo = ErasoftDbContext.Database.SqlQuery<QOH_QOO_ALL_ITEM>("SELECT * FROM [QOH_QOO_ALL_ITEM]").ToList();
-            var stf02Filter = ErasoftDbContext.Database.SqlQuery<PenjualanBarang>("select c.brg as KodeBrg,isnull(c.nama, '') + ' ' + isnull(c.nama2, '') as NamaBrg,c.KET_SORT1 as Kategori,c.KET_SORT2 as Merk, c.HJUAL as HJual from stf02 c left join sot01b b on c.brg = b.brg left join sot01a a on b.no_bukti = a.no_bukti where a.tgl >= dateadd(month, -3, getdate()) and isnull(b.brg, '') <> ''").ToList();
+            var stf02Filter = ErasoftDbContext.Database.SqlQuery<PenjualanBarang>("select c.brg as KodeBrg,isnull(c.nama, '') + ' ' + isnull(c.nama2, '') as NamaBrg,c.KET_SORT1 as Kategori,c.KET_SORT2 as Merk, c.HJUAL as HJual from stf02 c left join (select distinct brg from sot01a a inner join sot01b b on a.no_bukti = b.no_bukti where a.tgl_input >= dateadd(month, -3, getdate())) b on c.brg = b.brg where isnull(b.brg, '') <> ''").ToList();
             foreach(var barang in stf02Filter)
             {
                 var getQoh = 0d;
