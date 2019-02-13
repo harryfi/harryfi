2f1f9865f0170a9caaed0f950fa4869d546c550a
suwantatri <fierywings5@gmail.com>
2019-02-13 16:13:31 +0700

    viewmodel account

---
+++ MasterOnline/ViewModels/MenuAccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterOnline.Models;
+
+namespace MasterOnline.ViewModels
+{
+    public class MenuAccountViewModel
+    {
+        public List<Account> ListAccount { get; set; } = new List<Account>();
+        public List<Partner> ListPartner { get; set; } = new List<Partner>();
+    }
+}
