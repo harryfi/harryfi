37e3c454448625f2d75e75eedc909b49916c5787
suwantatri <fierywings5@gmail.com>
2019-03-06 15:11:34 +0700

    revisi subject email

--- MasterOnline/Controllers/AccountController.cs
+++ MasterOnline/Controllers/AccountController.cs
@@ -597,7 +597,7 @@ namespace MasterOnline.Controllers
             var message = new MailMessage();
             message.To.Add(email);
             message.From = new MailAddress("csmasteronline@gmail.com");
-            message.Subject = "Pendaftaran MasterOnline berhasil!";
+            message.Subject = "Pendaftaran Master Online berhasil!";
             message.Body = string.Format(body, account.Email, originPassword, nama);
             message.IsBodyHtml = true;
 #if AWS
