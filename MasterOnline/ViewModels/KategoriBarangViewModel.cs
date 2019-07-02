36c7b6291cae8c92b18f06c066cecb032e432da0
DheAfifah <nurulafifah5996@gmail.com>
2019-07-02 14:38:08 +0700

    delete poin 2 support; perbaikan save kategori, merk, supplier dan gudang

--- MasterOnline/ViewModels/KategoriBarangViewModel.cs
+++ MasterOnline/ViewModels/KategoriBarangViewModel.cs
@@ -10,5 +10,8 @@ namespace MasterOnline.ViewModels
     {
         public STF02E Kategori { get; set; }
         public List<STF02E> ListKategori { get; set; } = new List<STF02E>();
+        //add by nurul 2/7/2019
+        public List<string> Errors { get; set; } = new List<string>();
+        //end add by nurul 2/7/2019
     }
 }
