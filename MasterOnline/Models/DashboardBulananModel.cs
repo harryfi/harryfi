006afd5316bcef7148bb54f808b8f48a4d947cfe
ErasoftTeknologi <Erasoftt@gmail.com>
2019-07-26 16:35:48 +0700

    Revert "create query di controller"
    This reverts commit f9ff9ee28841b4f0b369cc819bb2d73b83b00433.

--- MasterOnline/Models/DashboardBulananModel.cs
+++ MasterOnline/Models/DashboardBulananModel.cs
@@ -7,13 +7,10 @@ namespace MasterOnline.Models
 {
     public class DashboardBulananModel
     {
-        //public string NamaMinggu { get; set; }
-        //public string Minggu1 { get; set; }
-        //public string Minggu2 { get; set; }
-        //public string Minggu3 { get; set; }
-        //public string Minggu4 { get; set; }
-        public string No { get; set; }
-        public string Jumlah { get; set; }
-        public string Nilai { get; set; }
+        public string NamaMinggu { get; set; }
+        public string Minggu1 { get; set; }
+        public string Minggu2 { get; set; }
+        public string Minggu3 { get; set; }
+        public string Minggu4 { get; set; }
     }
 }
