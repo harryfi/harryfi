74bf6937992f940261f60a73587006939204ea61
ErasoftTeknologi <Erasoftt@gmail.com>
2019-08-22 16:41:17 +0700

    Bug Fix Lazada saat update stok tidak throw exception jika fail

--- MasterOnline/Controllers/StokControllerJob.cs
+++ MasterOnline/Controllers/StokControllerJob.cs
@@ -1165,13 +1165,17 @@ namespace MasterOnline.Controllers
                 else
                 {
                     ret.message = res.detail[0].message;
+                    throw new Exception(ret.message);
                     //currentLog.REQUEST_EXCEPTION = res.detail[0].message;
                     //manageAPI_LOG_MARKETPLACE(api_status.Failed, ErasoftDbContext, token, currentLog);
                 }
             }
             catch (Exception ex)
             {
-                ret.message = ex.ToString();
+                string msg = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+                //ret.message = ex.ToString();
+                throw new Exception(msg);
+
                 //currentLog.REQUEST_EXCEPTION = ex.Message;
                 //manageAPI_LOG_MARKETPLACE(api_status.Exception, ErasoftDbContext, token, currentLog);
             }
