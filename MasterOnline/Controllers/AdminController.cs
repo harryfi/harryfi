5b9252da3d9f64b288e20bc43dfdf17c9c6ec48f
DheAfifah <nurulafifah5996@gmail.com>
2019-08-27 13:29:52 +0700

    perbaikan send email sub

--- MasterOnline/Controllers/AdminController.cs
+++ MasterOnline/Controllers/AdminController.cs
@@ -599,7 +599,7 @@ namespace MasterOnline.Controllers
                 var cekPayment = MoDbContext.AktivitasSubscription.Where(a => a.Email == vm.Payment.Email && a.TanggalBayar == vm.Payment.TanggalBayar && a.Nilai == vm.Payment.Nilai).ToList();
                 if (cekPayment != null)
                 {
-                    await SendInvoice(cekPayment.Single().RecNum, "0");
+                    await SendInvoice(Convert.ToString(cekPayment.Single().RecNum), "0");
                 }
             }
 
@@ -614,29 +614,30 @@ namespace MasterOnline.Controllers
         }
 
         //add by nurul 12/8/2019, kirim invoice lewat email 
-        public async Task<ActionResult> SendInvoice(int? aktSubID, string btnKirim)
+        //public async Task<ActionResult> SendInvoice(int? aktSubID, string btnKirim)
+        public async Task<ActionResult> SendInvoice(string aktSubID, string btnKirim)
         {
             try
             {
                 var ambilUlangAktSub = new AktivitasSubscription();
                 bool succes = false;
-                var aktSubId = Convert.ToInt64(aktSubID);
+                var aktSubId = Convert.ToInt32(aktSubID);
                 var aktSub = MoDbContext.AktivitasSubscription.Single(u => u.RecNum == aktSubId);
                 if (aktSub.Invoice_No == null || aktSub.Invoice_No.Substring(3, 4) == "2019") //kalo Invoice_No null/masih format lama
                 {
                     var sub = MoDbContext.Subscription.Single(u => u.KODE == aktSub.TipeSubs).KETERANGAN;
 
                     var listAktSubInDb = MoDbContext.AktivitasSubscription.OrderBy(p => p.RecNum).ToList();
+                    var cekListSudahAdaNoInv = MoDbContext.AktivitasSubscription.Where(a => a.Invoice_No != null && a.Invoice_No.Substring(3, 4) != "2019").OrderBy(p => p.RecNum).ToList();
                     var digitAkhir = "";
                     var noInv = "";
-
-                    if (listAktSubInDb.Count == 0)
+                    //if (listAktSubInDb.Count == 0)
+                    if (cekListSudahAdaNoInv.Count() == 0)
                     {
                         digitAkhir = "0001";
                         noInv = $"MO/{DateTime.Now.Year.ToString().Substring(2, 2)}/{digitAkhir}";
-                        MoDbContext.Database.ExecuteSqlCommand("DBCC CHECKIDENT (AktivitasSubscription, RESEED, 0)");
                     }
-                    else
+                    else 
                     {
                         //var lastRecNum = listAktSubInDb.Last().RecNum;
                         //lastRecNum++;
@@ -726,6 +727,7 @@ namespace MasterOnline.Controllers
                 else if (succes == false)
                 {
                     ViewData["SuccessMessage"] = $"Pembayaran {ambilUlangAktSub.Account} gagal kirim email.";
+                    return RedirectToAction("AktivitasSubscription");
                 }
                 return new EmptyResult();
             }
