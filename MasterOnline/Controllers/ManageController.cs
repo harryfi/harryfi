e162204a24c72bcc82b93d7e60fca9eb108aca86
ErasoftTeknologi <Erasoftt@gmail.com>
2019-09-06 20:49:14 +0700

    harga = qty * h_satuan

--- MasterOnline/Controllers/ManageController.cs
+++ MasterOnline/Controllers/ManageController.cs
@@ -21799,7 +21799,7 @@ namespace MasterOnline.Controllers
                             SATUAN = "2",
                             H_SATUAN = Convert.ToDouble(faktur.PriceRp.Replace("Rp ", "").Replace(".", "")),
                             QTY = Convert.ToDouble(faktur.Quantity),
-                            HARGA = Convert.ToDouble(faktur.PriceRp.Replace("Rp ", "").Replace(".", "")),
+                            HARGA = Convert.ToDouble(faktur.Quantity) * Convert.ToDouble(faktur.PriceRp.Replace("Rp ", "").Replace(".", "")),
                             QTY_KIRIM = 0,
                             QTY_RETUR = 0,
                             GUDANG = "001" //buat default gudang 001, untuk semua akun baru
@@ -22479,7 +22479,7 @@ namespace MasterOnline.Controllers
                             SATUAN = "2",
                             H_SATUAN = Convert.ToDouble(faktur.HargaProduk.Replace("Rp ", "").Replace(".", "")),
                             QTY = Convert.ToDouble(faktur.JumlahProduk),
-                            HARGA = Convert.ToDouble(faktur.HargaProduk.Replace("Rp ", "").Replace(".", "")),
+                            HARGA = Convert.ToDouble(faktur.JumlahProduk) * Convert.ToDouble(faktur.HargaProduk.Replace("Rp ", "").Replace(".", "")),
                             QTY_KIRIM = 0,
                             QTY_RETUR = 0,
                             GUDANG = "001" //buat default gudang 001, untuk semua akun baru
