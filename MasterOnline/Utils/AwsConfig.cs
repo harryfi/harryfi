3052b29de4a4fa802c7a434b2fec1b8200edd828
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-03-20 19:06:19 +0700

    Update

--- MasterOnline/Utils/AwsConfig.cs
+++ MasterOnline/Utils/AwsConfig.cs
@@ -12,5 +12,9 @@ namespace MasterOnline.Utils
         public static string _bucketName { get; set; } = "masteronlinebucket";
         public static string _amazonS3PublicUrl { get; set; } = "https://masteronlinebucket.s3-ap-southeast-1.amazonaws.com/";
         public static string _amazonAwsUrl { get; set; } = "https://s3-ap-southeast-1.amazonaws.com/";
+
+        //for uploadfileservice.cs
+        public static string _bucketFileName { get; set; } = "bucket-backup-db";
+        public static string _amazonS3PrivateUrl { get; set; } = "https://bucket-backup-db.s3-ap-southeast-1.amazonaws.com/";
     }
 }
