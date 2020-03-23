6d9e93ffac154b8a8e44f49ad4f5f2bab19db642
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-03-23 14:29:35 +0700

    Save

--- MasterOnline/Controllers/ManageController.cs
+++ MasterOnline/Controllers/ManageController.cs
@@ -39,6 +39,12 @@ using Hangfire.SqlServer;
 using CsvHelper;
 //end add by calvin 7 april 2019
 
+// add by fauzi 
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+// end
+
 using Spire.Xls;
 
 namespace MasterOnline.Controllers
@@ -38827,7 +38833,64 @@ namespace MasterOnline.Controllers
             var progress = 0;
             try
             {
-                UploadFileServices.UploadFile("~/Content/Uploaded/ERASOFT_rahmamk_BayarBukalapak_20191125_1040480377.csv");
+                for (int file_index = 0; file_index < Request.Files.Count; file_index++)
+                {
+                    var file = Request.Files[file_index];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var UrlfileUploaded = UploadFileServices.UploadFile(file);
+
+                        string extension = Path.GetExtension(file.FileName);
+                        string conString = string.Empty;
+                        switch (extension)
+                        {
+                            case ".xls": //Excel 97-03.
+                                conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                                break;
+                            case ".xlsx": //Excel 07 and above.
+                                conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                                break;
+                        }
+
+                        conString = string.Format(conString, UrlfileUploaded);
+
+                        using (OleDbConnection connExcel = new OleDbConnection(conString))
+                        {
+                            using (OleDbCommand cmdExcel = new OleDbCommand())
+                            {
+                                using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                                {
+                                    DataTable dt = new DataTable();
+                                    cmdExcel.Connection = connExcel;
+
+                                    //Get the name of First Sheet.
+                                    connExcel.Open();
+                                    DataTable dtExcelSchema;
+                                    dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                                    string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                    connExcel.Close();
+
+                                    //Read Data from First Sheet.
+                                    connExcel.Open();
+                                    cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                    odaExcel.SelectCommand = cmdExcel;
+                                    odaExcel.Fill(dt);
+                                    connExcel.Close();
+
+                                    //foreach (DataRow row in dt.Rows)
+                                    //{
+                                    //    customers.Add(new CustomerModel
+                                    //    {
+                                    //        CustomerId = Convert.ToInt32(row["Id"]),
+                                    //        Name = row["Name"].ToString(),
+                                    //        Country = row["Country"].ToString()
+                                    //    });
+                                    //}
+                                }
+                            }
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
--- MasterOnline/Services/UploadFileServices.cs
+++ MasterOnline/Services/UploadFileServices.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Web;
+using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using MasterOnline.Utils;
+using Amazon.S3.Transfer;
+using System.Threading.Tasks;
+
+
 
 namespace MasterOnline.Services
 {
@@ -10,31 +17,38 @@ namespace MasterOnline.Services
         private static readonly string _awsAccessKey = AwsConfig._awsAccessKey;
         private static readonly string _awsSecretKey = AwsConfig._awsSecretKey;
         private static readonly string _bucketName = AwsConfig._bucketFileName;
-        private static readonly string _amazonS3PublicUrl = AwsConfig._amazonS3PrivateUrl;
+        private static readonly string _amazonS3PublicUrl = AwsConfig._amazonS3PublicUrl;
         private static readonly string _amazonAwsUrl = AwsConfig._amazonAwsUrl;
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.APSoutheast1;
+        private const string keyName = "upload_1";
+        private static IAmazonS3 s3Client;
+        
 
-        private static AmazonS3Client client;
-
-        public UploadFileServices()
+        public static string UploadFile(HttpPostedFileBase file)
         {
-            client = new AmazonS3Client(Amazon.RegionEndpoint.APSoutheast1);
-        }
+            var fileName = Guid.NewGuid().ToString();
+            var ret = "";
 
-        public static void UploadFile(string filePath)
-        {
-            var result = "";
             try
             {
-                PutObjectRequest putRequest = new PutObjectRequest
-                {
-                    BucketName = _bucketName,
-                    Key = _awsAccessKey,
-                    FilePath = filePath,
-                    ContentType = "plain/text"
-                };
+                Stream inputSteram = file.InputStream;
 
-                PutObjectResponse response = client.PutObject(putRequest);
-                result = response.ResponseMetadata.Metadata.Values.ToString();
+                IAmazonS3 client;
+                using (client = new AmazonS3Client(_awsAccessKey, _awsSecretKey, Amazon.RegionEndpoint.APSoutheast1))
+                {
+                    PutObjectRequest putRequest = new PutObjectRequest
+                    {
+                        BucketName = _bucketName,
+                        Key = string.Format(file.FileName),
+                        //CannedACL = S3CannedACL.PublicRead,//PERMISSION TO FILE PUBLIC ACCESIBLE
+                        ContentType = file.ContentType,
+                        InputStream = inputSteram
+                    };
+
+                    ret = _amazonS3PublicUrl + string.Format(file.FileName);
+                    client.PutObject(putRequest);
+                }
+                            
             }
             catch (AmazonS3Exception amazonS3Exception)
             {
@@ -50,7 +64,7 @@ namespace MasterOnline.Services
                     //throw new Exception("Error occurred: " + amazonS3Exception.Message);
                 }
             }
-            //return result;
+            return ret;
         }
     }
 }
