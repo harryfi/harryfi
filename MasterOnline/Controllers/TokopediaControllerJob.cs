a0948f6a03b9eace984b27c281a8e193da120ce9
tri suwanta <fierywings5@gmail.com>
2020-05-04 14:21:53 +0700

    perbaikan interval pemanggilan api get order tokped

--- MasterOnline/Controllers/TokopediaControllerJob.cs
+++ MasterOnline/Controllers/TokopediaControllerJob.cs
@@ -2935,8 +2935,10 @@ namespace MasterOnline.Controllers
             while (daysFrom > -13)
             {
                 await GetOrderList3days(iden, stat, CUST, NAMA_CUST, 1, 0, daysFrom, daysTo);
-                daysFrom -= 3;
-                daysTo -= 3;
+                //daysFrom -= 3;
+                //daysTo -= 3;
+                daysFrom -= 2;
+                daysTo -= 2;
             }
             return ret;
         }
@@ -3085,8 +3087,10 @@ namespace MasterOnline.Controllers
             {
                 await GetOrderListCompleted3Days(iden, stat, CUST, NAMA_CUST, 1, 0, daysFrom, daysTo);
 
-                daysFrom -= 3;
-                daysTo -= 3;
+                //daysFrom -= 3;
+                //daysTo -= 3;
+                daysFrom -= 2;
+                daysTo -= 2;
             }
             return ret;
 
@@ -3230,8 +3234,10 @@ namespace MasterOnline.Controllers
             {
                 await GetOrderListCancel3days(iden, CUST, NAMA_CUST, 1, 0, daysFrom, daysTo);
 
-                daysFrom -= 3;
-                daysTo -= 3;
+                //daysFrom -= 3;
+                //daysTo -= 3;
+                daysFrom -= 2;
+                daysTo -= 2;
             }
             return ret;
         }
