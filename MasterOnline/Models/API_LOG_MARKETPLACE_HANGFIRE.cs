d9ad1f384337dc963228dd57105a09e316160e78
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-05-08 13:35:55 +0700

    save

--- MasterOnline/Models/API_LOG_MARKETPLACE_HANGFIRE.cs
+++ MasterOnline/Models/API_LOG_MARKETPLACE_HANGFIRE.cs
@@ -12,7 +12,7 @@ namespace MasterOnline
         public string AkunMarketplace { get; set; }
         public string REQUEST_CATEGORY { get; set; }
         public string REQUEST_ACTION { get; set; }
-        public DateTime REQUEST_DATETIME{ get; set; }
+        public DateTime REQUEST_DATETIME { get; set; }
         public string REQUEST_RESULT { get; set; }
         public string REQUEST_EXCEPTION { get; set; }
         public string REQUEST_ID { get; set; }
@@ -21,9 +21,9 @@ namespace MasterOnline
     {
         public string Username { get; set; }
         public string Email { get; set; }
-        public string NamaTokoOnline{ get; set; }
-        public string DatabaseSourceErasoft{ get; set; }
-        public string DatabasePathErasoft{ get; set; }
+        public string NamaTokoOnline { get; set; }
+        public string DatabaseSourceErasoft { get; set; }
+        public string DatabasePathErasoft { get; set; }
         public int HangfireServerCount { get; set; }
         public DateTime? LAST_LOGIN_DATE { get; set; }
         public DateTime? TGL_SUBSCRIPTION { get; set; }
