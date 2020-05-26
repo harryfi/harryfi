e80cbef08db8a445f83ebe7b6731d6489dfacb2f
DheAfifah <nurulafifah5996@gmail.com>
2020-05-26 17:54:43 +0700

    dicoba 4 pesanan stabil

--- MasterOnline/Controllers/TokopediaControllerJob.cs
+++ MasterOnline/Controllers/TokopediaControllerJob.cs
@@ -2495,7 +2495,7 @@ namespace MasterOnline.Controllers
             string responseFromServer = "";
             //try
             //{
-            using (WebResponse response = await myReq.GetResponseAsync())
+            using (WebResponse response = myReq.GetResponse())
             {
                 using (Stream stream = response.GetResponseStream())
                 {
