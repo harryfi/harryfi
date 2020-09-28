8660d331749ed8380745bd2255cf8fd19946d90c
tri suwanta <fierywings5@gmail.com>
2020-09-28 17:08:54 +0700

    inp. picking partial page

---
+++ MasterOnline/ViewModels/BarcodeViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MasterOnline.Models;
+
+
+namespace MasterOnline.ViewModels
+{
+    public class BarcodeViewModels
+    {
+    }
+
+    public class ScanBarcodePickingBarang
+    {
+        public string code { get; set; }
+        public int qty { get; set; }
+        public bool isValid { get; set; }
+    }
+    public class ScanBarcodePickingBarangViewModel
+    {
+        public string NO_PL { get; set; }
+        public int jmlBrg { get; set; }
+        public int maxBrg { get; set; }
+        public int jmlQty { get; set; }
+        public int maxQty { get; set; }
+        public List<ScanBarcodePickingBarang> dataScan { get; set; }
+    }
+}
