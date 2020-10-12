fca8e78c84199be552c441f68687404cf2f65d77
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-10-12 16:28:41 +0700

    fix addhour +7 remark in shopify

--- MasterOnline/Controllers/ShopifyControllerJob.cs
+++ MasterOnline/Controllers/ShopifyControllerJob.cs
@@ -548,8 +548,8 @@ namespace MasterOnline.Controllers
                                                     batchinsertItem = new List<TEMP_SHOPIFY_ORDERS_ITEM>();
 
                                                     //2020-04-08T05:12:41
-                                                    var dateOrder = Convert.ToDateTime(order.created_at).AddHours(7).ToString("yyyy-MM-dd HH:mm:ss");
-                                                    var datePay = Convert.ToDateTime(order.processed_at).AddHours(7).ToString("yyyy-MM-dd HH:mm:ss");
+                                                    var dateOrder = Convert.ToDateTime(order.created_at).ToString("yyyy-MM-dd HH:mm:ss");
+                                                    var datePay = Convert.ToDateTime(order.processed_at).ToString("yyyy-MM-dd HH:mm:ss");
                         
                                                     var shippingLine = "";
                                                     var trackingCompany = "";
