33aa2a42dbca0ff937c7463f8a6cb2b7ab34ece2
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-11-21 20:12:50 +0700

    save

--- MasterOnline/Controllers/ManageController.cs
+++ MasterOnline/Controllers/ManageController.cs
@@ -22202,15 +22202,32 @@ namespace MasterOnline.Controllers
                                 if (dataStf02h.BRG_MP.Contains(";"))
                                 {
                                     var gdnSku = dataStf02h.BRG_MP.Split(';');
-                                    var catatanSplitJDID = catatan_split[2].Split(';');
-                                    if (gdnSku[1] != catatanSplitJDID[1])
-                                        return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    if (catatan_split[2].Contains(";"))
+                                    {
+                                        var catatanSplitJDID = catatan_split[2].Split(';');
+                                        if (gdnSku[1] != catatanSplitJDID[1])
+                                            return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    }
+                                    else
+                                    {
+                                        if (gdnSku[1] != catatan_split[2])
+                                            return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    }
+
                                 }
                                 else
                                 {
-                                    var catatanSplitJDID = catatan_split[2].Split(';');
-                                    if (dataStf02h.BRG_MP != catatanSplitJDID[1])
-                                        return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    if (catatan_split[2].Contains(";"))
+                                    {
+                                        var catatanSplitJDID = catatan_split[2].Split(';');
+                                        if (dataStf02h.BRG_MP != catatanSplitJDID[1])
+                                            return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    }
+                                    else
+                                    {
+                                        if (dataStf02h.BRG_MP != catatan_split[2])
+                                            return JsonErrorMessage("Barang :" + dataStf02h.BRG + " sudah link dengan barang lain(kode barang mp :" + dataStf02h.BRG_MP + ")\nSilahkan lakukan unlink produk ini terlebih dahulu.");
+                                    }
                                 }
                             }
                             else
