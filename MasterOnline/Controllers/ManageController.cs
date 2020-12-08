0fdd7e9f3ab6077efb2a55d4c80aae18e8fc3862
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2020-12-08 21:27:53 +0700

    save

--- MasterOnline/Controllers/ManageController.cs
+++ MasterOnline/Controllers/ManageController.cs
@@ -4888,8 +4888,8 @@ namespace MasterOnline.Controllers
                     };
                     //Task.Run(() => jdAPI.checkAPICustomerShop(dataJD)).Wait();
                     //jdAPI.JDID_checkAPICustomerShop(dataJD);
-                    //jdAPI.JDID_checkAPICustomerShopNew(dataJD);
-                    Task.Run(() => jdAPI.JDID_checkAPICustomerShopNew(dataJD)).Wait();
+                    jdAPI.JDID_checkAPICustomerShopNew(dataJD);
+                    //Task.Run(() => jdAPI.JDID_checkAPICustomerShopNew(dataJD)).Wait();
                 }
 
             }
