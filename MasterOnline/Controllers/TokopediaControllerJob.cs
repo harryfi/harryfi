ddad09c593f5dbbf8500c976895f63acb31dfb38
tri suwanta <fierywings5@gmail.com>
2021-03-03 17:33:33 +0700

    create product back

--- MasterOnline/Controllers/TokopediaControllerJob.cs
+++ MasterOnline/Controllers/TokopediaControllerJob.cs
@@ -111,8 +111,8 @@ namespace MasterOnline.Controllers
             return ret;
         }
 
-        //[AutomaticRetry(Attempts = 0)]
-        [AutomaticRetry(Attempts = 0, DelaysInSeconds = new int[] { 300, 300, 300 })]
+        [AutomaticRetry(Attempts = 0)]
+        //[AutomaticRetry(Attempts = 0, DelaysInSeconds = new int[] { 300, 300, 300 })]
         [Queue("1_create_product")]
         [NotifyOnFailed("Create Product {obj} ke Tokopedia Gagal.")]
         public async Task<string> CreateProductGetStatus(string dbPathEra, string kodeProduk, string log_CUST, string log_ActionCategory, string log_ActionName, TokopediaAPIData iden, string brg, int upload_id, string log_request_id)
