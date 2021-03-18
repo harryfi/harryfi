3d7b586ff9a7778725753c04d949718bce3c3894
tri suwanta <fierywings5@gmail.com>
2021-03-18 15:36:19 +0700

    perbaikan filter logistik di create dan edit brg shopee

--- MasterOnline/Controllers/ShopeeControllerJob.cs
+++ MasterOnline/Controllers/ShopeeControllerJob.cs
@@ -5616,14 +5616,18 @@ namespace MasterOnline.Controllers
             //add by calvin 21 desember 2018, default nya semua logistic enabled
             var ShopeeGetLogisticsResult = await GetLogistics(iden);
 
-            foreach (var log in ShopeeGetLogisticsResult.logistics.Where(p => p.enabled == true && p.fee_type.ToUpper() != "CUSTOM_PRICE" && p.fee_type.ToUpper() != "SIZE_SELECTION"))
+            foreach (var log in ShopeeGetLogisticsResult.logistics.Where(p => p.enabled == true && p.fee_type.ToUpper() != "CUSTOM_PRICE" 
+            && p.fee_type.ToUpper() != "SIZE_SELECTION" && p.mask_channel_id == 0))
             {
                 bool lolosValidLogistic = true;
                 if (log.weight_limits != null)
                 {
-                    if (log.weight_limits.item_max_weight < (brgInDb.BERAT / 1000))
+                    if (log.weight_limits.item_max_weight > 0)
                     {
-                        lolosValidLogistic = false;
+                        if (log.weight_limits.item_max_weight < (brgInDb.BERAT / 1000))
+                        {
+                            lolosValidLogistic = false;
+                        }
                     }
                     if (log.weight_limits.item_min_weight > (brgInDb.BERAT / 1000))
                     {
@@ -7111,7 +7115,8 @@ namespace MasterOnline.Controllers
             //add by calvin 21 desember 2018, default nya semua logistic enabled
             var ShopeeGetLogisticsResult = await GetLogistics(iden);
 
-            foreach (var log in ShopeeGetLogisticsResult.logistics.Where(p => p.enabled == true && p.fee_type.ToUpper() != "CUSTOM_PRICE" && p.fee_type.ToUpper() != "SIZE_SELECTION"))
+            foreach (var log in ShopeeGetLogisticsResult.logistics.Where(p => p.enabled == true && p.fee_type.ToUpper() != "CUSTOM_PRICE" 
+            && p.fee_type.ToUpper() != "SIZE_SELECTION" && p.mask_channel_id == 0))
             {
                 //logistics.Add(new ShopeeLogisticsClass()
                 //{
@@ -7122,9 +7127,12 @@ namespace MasterOnline.Controllers
                 bool lolosValidLogistic = true;
                 if (log.weight_limits != null)
                 {
-                    if (log.weight_limits.item_max_weight < (brgInDb.BERAT / 1000))
+                    if (log.weight_limits.item_max_weight > 0)
                     {
-                        lolosValidLogistic = false;
+                        if (log.weight_limits.item_max_weight < (brgInDb.BERAT / 1000))
+                        {
+                            lolosValidLogistic = false;
+                        }
                     }
                     if (log.weight_limits.item_min_weight > (brgInDb.BERAT / 1000))
                     {
@@ -9437,6 +9445,7 @@ namespace MasterOnline.Controllers
             public bool enabled { get; set; }
             public int logistic_id { get; set; }
             public string fee_type { get; set; }
+            public int mask_channel_id { get; set; }
         }
 
         public class ShopeeGetLogisticsResultWeight_Limits
