d6833858058816340858b4597c3eb488d4a25df5
muhammadfauzimilad <muhammadfauzimilad213@gmail.com>
2021-04-19 15:38:42 +0700

    save

--- MasterOnline/Utils/ApplicationPreload.cs
+++ MasterOnline/Utils/ApplicationPreload.cs
@@ -14,26 +14,6 @@ namespace MasterOnline.Utils
         public void Preload(string[] parameters)
         {
 
-            //initialize log txt
-            #region Logging
-            string messageErrorLog = "";
-            string filename = "Log_Initial_AppPreload_" + DateTime.Now.AddHours(7).ToString("yyyyMMddhhmmss") + ".txt";
-            var path = Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("~/Content/log/"), filename);
-
-            if (!System.IO.File.Exists(path))
-            {
-                System.IO.Directory.CreateDirectory(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("~/Content/log/"), ""));
-                var asd = System.IO.File.Create(path);
-                asd.Close();
-            }
-            StreamWriter tw = new StreamWriter(path);
-            var msglog = "Log ApplicationPreload Running...... Pada waktu " + DateTime.Now.AddHours(7).ToString("yyyy-MM-dd hh:mm:ss");
-            tw.WriteLine(msglog);
-            tw.Close();
-            tw.Dispose();
-            #endregion
-
-
 #if (DEBUG || Debug_AWS)
             HangfireBootstrapper.Instance.Start();
 #else
