45f52851ac49ea3024a7cd918247afeb914d4c23
tri suwanta <fierywings5@gmail.com>
2021-10-22 16:21:19 +0700

    quick fix error di update stok, sinkro versi baru

--- MasterOnline/Controllers/StokControllerJob.cs
+++ MasterOnline/Controllers/StokControllerJob.cs
@@ -5943,18 +5943,18 @@ namespace MasterOnline.Controllers
             public string body_html { get; set; }
             public string vendor { get; set; }
             public string product_type { get; set; }
-            public DateTime created_at { get; set; }
+            //public DateTime created_at { get; set; }
             public string handle { get; set; }
-            public DateTime updated_at { get; set; }
-            public DateTime published_at { get; set; }
+            //public DateTime updated_at { get; set; }
+            //public DateTime published_at { get; set; }
             public string template_suffix { get; set; }
             public string published_scope { get; set; }
             public string tags { get; set; }
             public string admin_graphql_api_id { get; set; }
             public ShopifyGetItemDetailResultProductVariant[] variants { get; set; }
-            public ShopifyGetItemDetailResultProductOption[] options { get; set; }
-            public ShopifyGetItemDetailResultProductImageMore[] images { get; set; }
-            public ShopifyGetItemDetailResultProductImage image { get; set; }
+            //public ShopifyGetItemDetailResultProductOption[] options { get; set; }
+            //public ShopifyGetItemDetailResultProductImageMore[] images { get; set; }
+            //public ShopifyGetItemDetailResultProductImage image { get; set; }
         }
 
         public class ShopifyGetItemDetailResultProductImage
@@ -5977,29 +5977,29 @@ namespace MasterOnline.Controllers
             public long id { get; set; }
             public long product_id { get; set; }
             public string title { get; set; }
-            public string price { get; set; }
+            //public string price { get; set; }
             public string sku { get; set; }
-            public int position { get; set; }
+            //public int position { get; set; }
             public string inventory_policy { get; set; }
-            public object compare_at_price { get; set; }
+            //public object compare_at_price { get; set; }
             public string fulfillment_service { get; set; }
             public string inventory_management { get; set; }
             public string option1 { get; set; }
             public object option2 { get; set; }
             public object option3 { get; set; }
-            public DateTime created_at { get; set; }
-            public DateTime updated_at { get; set; }
-            public bool taxable { get; set; }
+            //public DateTime created_at { get; set; }
+            //public DateTime updated_at { get; set; }
+            //public bool taxable { get; set; }
             public string barcode { get; set; }
-            public int grams { get; set; }
-            public object image_id { get; set; }
-            public float weight { get; set; }
-            public string weight_unit { get; set; }
+            //public int grams { get; set; }
+            //public object image_id { get; set; }
+            //public float weight { get; set; }
+            //public string weight_unit { get; set; }
             public long inventory_item_id { get; set; }
-            public int inventory_quantity { get; set; }
-            public int old_inventory_quantity { get; set; }
-            public bool requires_shipping { get; set; }
-            public string admin_graphql_api_id { get; set; }
+            //public int inventory_quantity { get; set; }
+            //public int old_inventory_quantity { get; set; }
+            //public bool requires_shipping { get; set; }
+            //public string admin_graphql_api_id { get; set; }
         }
 
         public class ShopifyGetItemDetailResultProductOption
