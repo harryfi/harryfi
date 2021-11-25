34979546af74c9b86b0b59c16c461c898c456813
tri suwanta <fierywings5@gmail.com>
2021-11-25 16:10:46 +0700

    inp. ui tabel

---
+++ MasterOnline/ViewModels/BindingMatrixBarang.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterOnline.ViewModels
+{
+    public class BindingMatrixBarang
+    {
+        public List<DataMatrixBarang> barang { get; set; }
+        public List<ARF01> akun { get; set; }
+    }
+    public class DataMatrixBarang
+    {
+        public string BRG { get; set; }
+        public string NAMABRG { get; set; }
+        public string BRG_MP { get; set; }
+        public string LINK { get; set; }
+        public int IDMARKET { get; set; }
+        public int RECNUM { get; set; }
+    }
+}
