9fd203621d0fcfcf4a537628c44995ebf66e4f7b
baimsky <baimsky@gmail.com>
2022-07-05 17:45:39 +0700

    handle retry insert stt01b

--- MasterOnline/Controllers/PartnerApiControllerJob.cs
+++ MasterOnline/Controllers/PartnerApiControllerJob.cs
@@ -537,7 +537,8 @@ namespace MasterOnline.Controllers
                                             string ret = retryInsertStt01b(email, "08. Insert STT01B_1", noStok, error, exec, json, 1);
                                             if (ret != "RETRY")
                                             {
-                                                return ey.Message;
+                                                continue;
+                                                //return ey.Message;
                                             }
                                             //}
                                         }
@@ -609,7 +610,8 @@ namespace MasterOnline.Controllers
                                             string ret = retryInsertStt01b(email, "08. Insert STT01B_1", noStok, error, exec, json, 1);
                                             if (ret != "RETRY")
                                             {
-                                                return ey.Message;
+                                                continue;
+                                                //return ey.Message;
                                             }
                                             //}
                                         }
